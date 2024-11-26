793eea28ec2b286009e4e32e556470bf37930c53
Mohamed _Saleh <127530951+mohamedsalehosman1@users.noreply.github.com>
2024-11-26 22:39:54 +0200

    Merge branch 'main' of https://github.com/mohamedsalehosman1/Plus2

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,10 +1,10 @@
 <?php
 
+use Laravel\Sanctum\Sanctum;
 
 return [
 
     /*
-
     |--------------------------------------------------------------------------
     | Stateful Domains
     |--------------------------------------------------------------------------
@@ -18,7 +18,7 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        // Sanctum::currentApplicationUrlWithPort()
+        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
