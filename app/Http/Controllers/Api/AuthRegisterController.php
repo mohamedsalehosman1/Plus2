9e2279537b289d0f00b42cf0fbacd6ada7f13c9b
Mohamed _Saleh <127530951+mohamedsalehosman1@users.noreply.github.com>
2024-11-26 17:31:51 +0200

    app

--- app/Http/Controllers/Api/AuthRegisterController.php
+++ app/Http/Controllers/Api/AuthRegisterController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-USE Hash;
+use Hash;
 use App\Http\Requests\Api\RegisterRequest;
+
 class AuthRegisterController extends Controller
 {
 
-public function register(RegisterRequest $request)  {
-$newuser= $request->validated();
-$user = User::create($newuser);
-$sucess['token']=$user->createToken('user',['app:all'])->plainTextToken;
-return response()->json('sucess', 200);
-}}
+    public function register(RegisterRequest $request)
+    {
+        $newuser = $request->validated();
+        $user = User::create($newuser);
+        $sucess['token'] = $user->createToken('user', ['app:all'])->plainTextToken;
+        return response()->json('sucess', 200);
+    }
+}
